e8bbfeedf84aa71d69107e6211ed8f45e87d135a
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-10 13:30:14 +0200

    Added navigation properties to GoogleCustomSearchItem model

---
+++ CustomSearchEngine/Migrations/20210110112741_AddGoogleCustomSearchItemNavigationProperties.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomSearchEngine.Migrations
+{
+    public partial class AddGoogleCustomSearchItemNavigationProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GoogleCustomSearchItems_GoogleCustomSearchRootObjects_GoogleCustomSearchRootObjectId",
+                table: "GoogleCustomSearchItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GoogleCustomSearchRootObjectId",
+                table: "GoogleCustomSearchItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GoogleCustomSearchItems_GoogleCustomSearchRootObjects_GoogleCustomSearchRootObjectId",
+                table: "GoogleCustomSearchItems",
+                column: "GoogleCustomSearchRootObjectId",
+                principalTable: "GoogleCustomSearchRootObjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GoogleCustomSearchItems_GoogleCustomSearchRootObjects_GoogleCustomSearchRootObjectId",
+                table: "GoogleCustomSearchItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GoogleCustomSearchRootObjectId",
+                table: "GoogleCustomSearchItems",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GoogleCustomSearchItems_GoogleCustomSearchRootObjects_GoogleCustomSearchRootObjectId",
+                table: "GoogleCustomSearchItems",
+                column: "GoogleCustomSearchRootObjectId",
+                principalTable: "GoogleCustomSearchRootObjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- CustomSearchEngine/Models/GoogleCustomSearchItem.cs
+++ CustomSearchEngine/Models/GoogleCustomSearchItem.cs
@@ -6,5 +6,7 @@
         public string Title { get; set; }
         public string Link { get; set; }
         public string Snippet { get; set; }
+        public int GoogleCustomSearchRootObjectId { get; set; }
+        public GoogleCustomSearchRootObject GoogleCustomSearchRootObjects { get; set; }
     }
 }
