354915f4972b71dd8b5ac03d58e26d970f7022d3
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-10 17:51:01 +0200

    Added SearchResult and SearchResultItem models

---
+++ CustomSearchEngine/Models/SearchResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomSearchEngine.Models
+{
+    public class SearchResult
+    {
+        public int Id { get; set; }
+        public string SearchQuery { get; set; }
+        public List<SearchResultItem> SearchResultItems { get; set; }
+    }
+}
