1bab6dc432537df20e535281ead3ec095cec20af
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-10 18:04:15 +0200

    Added CustomSearchEngineContext
    The following entities have been added:
     - SearchResults
     - SearchResultItems

---
+++ CustomSearchEngine/Models/CustomSearchEngineContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomSearchEngine.Models
+{
+    public class CustomSearchEngineContext : DbContext
+    {
+        public CustomSearchEngineContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<SearchResult> SearchResults { get; set; }
+        public DbSet<SearchResultItem> SearchResultItems { get; set; }
+    }
+}
