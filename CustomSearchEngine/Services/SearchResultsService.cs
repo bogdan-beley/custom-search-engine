c6e732ed2f9ab990898f2845721040815470b8ab
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-11 01:11:37 +0200

    Added FindByTitle method to SearchResultsService class and ISearchResultsService interface in Services

--- CustomSearchEngine/Services/SearchResultsService.cs
+++ CustomSearchEngine/Services/SearchResultsService.cs
@@ -1,5 +1,8 @@
 ﻿using CustomSearchEngine.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomSearchEngine.Services
 {
@@ -10,6 +13,11 @@ namespace CustomSearchEngine.Services
         public SearchResultsService(CustomSearchEngineContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public async Task<List<SearchResultItem>> FindByTitle(string searchQuery)
+        {
+            return await _dbContext.SearchResultItems.Where(x => x.Title.Contains(searchQuery)).ToListAsync();
         }
 
         public async Task WriteToDbAsync(SearchResult searchResult)
