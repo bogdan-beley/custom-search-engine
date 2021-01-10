e7d4990174dc63b69a10795a7be35d94c7dc88ae
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-10 18:53:25 +0200

    Created interface ISearchResultsService in Services

---
+++ CustomSearchEngine/Services/ISearchResults.cs
@@ -0,0 +1,10 @@
+﻿using CustomSearchEngine.Models;
+using System.Threading.Tasks;
+
+namespace CustomSearchEngine.Services
+{
+    public interface ISearchResultsService
+    {
+        Task WriteToDbAsync(SearchResult searchResult);
+    }
+}
