c4f13adc52b0fa22ca33b0c213b624dd934d340a
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-20 23:04:06 +0200

    Added BingCustomSearchService and IBingCustomSearchService interface to Services

---
+++ CustomSearchEngine/Services/BingCustomSearchService.cs
@@ -0,0 +1,46 @@
+﻿using CustomSearchEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CustomSearchEngine.Services
+{
+    public sealed class BingCustomSearchService : IBingCustomSearchService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BingCustomSearchService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<SearchResult> GetSearchResultsAsync(string searchQuery)
+        {
+            var results = await _httpClient
+                .GetFromJsonAsync<BingCustomSearchRootObject>(_httpClient.BaseAddress + searchQuery);
+
+            var searhResultItems = new List<SearchResultItem>();
+            foreach (var item in results.WebPages.Value)
+            {
+                searhResultItems.Add(new SearchResultItem()
+                {
+                    Title = item.Name,
+                    Link = item.DisplayUrl,
+                    Snippet = item.Snippet
+                });
+            }
+
+            var searchResult = new SearchResult()
+            {
+                SearchResultItems = searhResultItems,
+                SearchQuery = searchQuery,
+                SearchEngine = "Bing"
+            };
+
+            return searchResult;
+        }
+    }
+}
