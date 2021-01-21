3e0f6d44ac56582b2351530b1e2463065af1622a
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-21 21:56:52 +0200

    Added ExternalApiClientsConfig class

---
+++ CustomSearchEngine/Configuration/ExternalApiClientsConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomSearchEngine.Configuration
+{
+    public class ExternalApiClientsConfig
+    {
+        public string Url { get; set; }
+    }
+}
