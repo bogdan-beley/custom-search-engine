d1addca02be22b89d4331cde2cc4b3d3061bd01e
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-24 23:01:49 +0200

    Added 'ApiKey' property to ExternalApiClientsConfig class

--- CustomSearchEngine/Configuration/ExternalApiClientsConfig.cs
+++ CustomSearchEngine/Configuration/ExternalApiClientsConfig.cs
@@ -11,5 +11,6 @@ namespace CustomSearchEngine.Configuration
         public const string GoogleWebSearchApiClient = "GoogleWebSearchApiClient";
 
         public string Url { get; set; }
+        public string ApiKey { get; set; }
     }
 }
