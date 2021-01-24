cb4a4a7a41341c436b16781fe5ea53fac7447acd
Bogdan Beley <bogdan.beley@outlook.com>
2021-01-25 00:15:40 +0200

    Registered KeyVaultSecretManager configuration provider in Program.cs

--- CustomSearchEngine/Program.cs
+++ CustomSearchEngine/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -21,6 +24,17 @@ namespace CustomSearchEngine
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var config = builder.Build();
+                    var tokenProvider = new AzureServiceTokenProvider();
+
+                    var kvClient = new KeyVaultClient((authority, resource, scope) =>
+                        tokenProvider.KeyVaultTokenCallback(authority, resource, scope));
+
+                    builder.AddAzureKeyVault(config["AzureKeyVault:BaseUrl"], kvClient,
+                        new DefaultKeyVaultSecretManager());
                 });
     }
 }
